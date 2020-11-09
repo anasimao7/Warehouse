74518f0f5aa65ce5bd59fc03a3f8167acd67770b
Ana Simão <anasimaocontacto@gmail.com>
2020-11-09 17:48:22 +0000

    Small Application

---
+++ pack-download/2/2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace _2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cRUDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.encomendasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relacaoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Pesquisa = new System.Windows.Forms.Button();
+            this.submit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 127);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(396, 183);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cRUDToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(429, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cRUDToolStripMenuItem
+            // 
+            this.cRUDToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientesToolStripMenuItem,
+            this.encomendasToolStripMenuItem,
+            this.produtosToolStripMenuItem,
+            this.relacaoToolStripMenuItem});
+            this.cRUDToolStripMenuItem.Name = "cRUDToolStripMenuItem";
+            this.cRUDToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.cRUDToolStripMenuItem.Text = "CRUD";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            this.clientesToolStripMenuItem.Click += new System.EventHandler(this.clientesToolStripMenuItem_Click);
+            // 
+            // encomendasToolStripMenuItem
+            // 
+            this.encomendasToolStripMenuItem.Name = "encomendasToolStripMenuItem";
+            this.encomendasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.encomendasToolStripMenuItem.Text = "Encomendas";
+            this.encomendasToolStripMenuItem.Click += new System.EventHandler(this.encomendasToolStripMenuItem_Click);
+            // 
+            // produtosToolStripMenuItem
+            // 
+            this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.produtosToolStripMenuItem.Text = "Produtos";
+            this.produtosToolStripMenuItem.Click += new System.EventHandler(this.produtosToolStripMenuItem_Click);
+            // 
+            // relacaoToolStripMenuItem
+            // 
+            this.relacaoToolStripMenuItem.Name = "relacaoToolStripMenuItem";
+            this.relacaoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.relacaoToolStripMenuItem.Text = "Relacao";
+            this.relacaoToolStripMenuItem.Click += new System.EventHandler(this.relacaoToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Codencomenda";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Pesquisa
+            // 
+            this.Pesquisa.Location = new System.Drawing.Point(230, 99);
+            this.Pesquisa.Name = "Pesquisa";
+            this.Pesquisa.Size = new System.Drawing.Size(75, 23);
+            this.Pesquisa.TabIndex = 4;
+            this.Pesquisa.Text = "Pesquisa";
+            this.Pesquisa.UseVisualStyleBackColor = true;
+            this.Pesquisa.Click += new System.EventHandler(this.Pesquisa_Click);
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(311, 99);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 5;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 344);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.Pesquisa);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cRUDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem encomendasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relacaoToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Pesquisa;
+        private System.Windows.Forms.Button submit;
+    }
+}
+
---
+++ pack-download/2/2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        string constr;
+        SqlConnection con;
+        SqlDataAdapter dataAdapter;
+        DataTable table;
+
+        public Form1()
+        {
+            InitializeComponent();
+            constr = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=Armazem2";
+            con = new SqlConnection(constr);
+        }
+
+        private void Bind(string sql)
+        {
+            dataAdapter = new SqlDataAdapter(sql, constr);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+            table = new DataTable();
+            dataAdapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bind("select * from Clientes");
+        }
+
+        private void encomendasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bind("select * from Encomendas");
+        }
+
+        private void produtosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bind("select * from Produtos");
+        }
+
+        private void relacaoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bind("select * from Relacao");
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            dataAdapter.Update(table);
+        }
+
+        private void Pesquisa_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            Bind("select data, designacao, quantidade from Encomendas join Relacao on codencomenda = codencomendaFK join Produtos on codproduto = codprodutoFK where codencomenda = " + a);
+        }
+    }
+}
